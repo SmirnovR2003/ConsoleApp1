94180d721e601d8b0245083aff76829fcc318ba8
m.smm2003mail.ru@gmail.com <m.smm2003mail.ru@gmail.com>
2022-02-19 16:41:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using LearnCSharp;
+using System;
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(value: $"Вычисляет площадь и длину эллипса с полуосями A = {Сalculation.semiaxeA} и B = {Сalculation.semiaxeB}");
+
+        Сalculation calculation = new Сalculation();
+        int square = calculation.GetSquare();
+        int length = calculation.GetLength();
+        Console.WriteLine($"Площадь эллипса: {square}pi");
+        Console.WriteLine($"Длина эллипса: {length}pi");
+
+    }
+}
---
+++ ConsoleApp1/Сalculation.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace LearnCSharp
+{
+    public class Сalculation
+    {
+
+        public static int semiaxeA = 10;
+        public static int semiaxeB = 5;
+
+
+
+        public int GetSquare()
+        {
+            return semiaxeA * semiaxeB;
+        }
+        public int GetLength()
+        {
+            return semiaxeA + semiaxeB;
+        }
+    }
+}
